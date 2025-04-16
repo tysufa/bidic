503bb3bb1f76a0256cc5654874b37dbdb57f6a55
tysufa <philemon.penot@gmail.com>
2025-04-17 00:15:07 +0800

    added hdoc to the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include "lexer/lexer.hh"
 
+/// @brief Applies Foo's algorithm on x
+///
+/// This is a n-dimensional imaginary matrix transform that is
+/// that makes our flux capacitor work.
+///
+/// @param x the distilled essence of the matrix Z
+/// @returns The hypercombobulated entangled version of x
+int foobar(int x) { return x + 1; }
+
 int main(){
 
     Lexer tempo("int a = 3;");
