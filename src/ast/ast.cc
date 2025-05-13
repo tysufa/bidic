df5c601743af7bda78954b02ce69c0c5f7dec8e9
tysufa <philemon.penot@gmail.com>
2025-05-13 15:00:20 +0800

    added binary expression to ast, Evaluate is a placeholder

--- src/ast/ast.cc
+++ src/ast/ast.cc
@@ -1,29 +1,58 @@
 #include "ast.hh"
 #include "token.hh"
+#include <memory>
 
+std::unique_ptr<Literal> BinaryExpression::Evaluate() const {
+  std::unique_ptr<Literal> left = _left->Evaluate();
+  std::unique_ptr<Literal> right = _right->Evaluate();
+
+  LiteralValue left_val = left->value();
+  LiteralValue right_val = right->value();
+
+  // int *int_eval = std::get_if<int>(&left_val);
+  //
+  // if (int_eval) {
+  //   switch (_prefix_type) {
+  //   case TokenType::kMinus:
+  //     return std::make_unique<IntLiteral>(-(*int_eval));
+  //     break;
+  //
+  //   case TokenType::kTilde:
+  //     return std::make_unique<IntLiteral>(~(*int_eval));
+  //     break;
+  //
+  //   default:
+  //     throw std::runtime_error("Unknown prefix type: " +
+  //                              StringTokenType(_prefix_type));
+  //   }
+  // }
+  //
+  // else
+  //   return _expression_value->Evaluate();
+  return std::make_unique<IntLiteral>(0);
+}
 std::unique_ptr<Literal> PrefixExpression::Evaluate() const {
   std::unique_ptr<Literal> eval = _expression_value->Evaluate();
 
   LiteralValue eval_val = eval->value();
   // if evaluated prefix is an int, int_eval is an int *, else it's a nullptr
-  int* int_eval = std::get_if<int>(&eval_val);
+  int *int_eval = std::get_if<int>(&eval_val);
 
   // auto int_eval = static_cast<const IntLiteral*>(eval.get());
 
-
   if (int_eval) {
     switch (_prefix_type) {
-      case TokenType::kMinus:
-        return std::make_unique<IntLiteral>(-(*int_eval));
-        break;
+    case TokenType::kMinus:
+      return std::make_unique<IntLiteral>(-(*int_eval));
+      break;
 
-      case TokenType::kTilde:
-        return std::make_unique<IntLiteral>(~(*int_eval));
-        break;
+    case TokenType::kTilde:
+      return std::make_unique<IntLiteral>(~(*int_eval));
+      break;
 
-      default:
-        throw std::runtime_error("Unknown prefix type: " + StringTokenType(
-                                     _prefix_type));
+    default:
+      throw std::runtime_error("Unknown prefix type: " +
+                               StringTokenType(_prefix_type));
     }
   }
 
--- src/ast/ast.hh
+++ src/ast/ast.hh
@@ -1,68 +1,62 @@
 #pragma once
 #include "token.hh"
 #include "types.hh"
-#include <variant>
-#include <vector>
 #include <memory>
 #include <string>
+#include <variant>
+#include <vector>
 
 using LiteralValue = std::variant<int, float, std::string>;
 
 // any element executable code : Statement, Declaration, Expression, etc...
 class Instruction {
- public:
+public:
   virtual std::string TypeInstruction() const = 0;
 
- private:
-
+private:
 };
 
 // executable units : if, while, return
 class Statement : public Instruction {
- public:
+public:
   Statement() = default;
 
- private:
-
+private:
 };
 
 class Identifier {
- public:
-  Identifier(const std::string& name)
-    : _name(name) {}
+public:
+  Identifier(const std::string &name) : _name(name) {}
 
-  const std::string& name() const {
-    return _name;
-  }
+  const std::string &name() const { return _name; }
 
- private:
+private:
   std::string _name;
 };
 
 class Literal {
- public:
+public:
   virtual std::string DebugResult() const = 0;
 
   virtual LiteralValue value() const = 0;
-
 };
 
 class IntLiteral : public Literal {
- public:
-  IntLiteral(int val)
-    : _value(val) {}
+public:
+  IntLiteral(int val) : _value(val) {}
 
   std::string DebugResult() const override { return std::to_string(_value); }
 
   LiteralValue value() const override { return _value; }
 
- private:
+private:
   int _value;
 };
 
-// combination of variables, constants, operators and function calls that evaluate to a single value
+// combination of variables, constants, operators and function calls that
+// evaluate to a single value
 class Expression {
- public:
+public:
   virtual ~Expression() = default;
 
   virtual std::unique_ptr<Literal> Evaluate() const = 0;
@@ -70,96 +64,107 @@ class Expression {
 };
 
 class IntExpression : public Expression {
- public:
-  IntExpression(int value)
-    : _value(value) {}
+public:
+  IntExpression(int value) : _value(value) {}
 
   std::unique_ptr<Literal> Evaluate() const override {
     return std::make_unique<IntLiteral>(_value);
   };
 
-  std::string ExpressionType() const override {return "Int";}
+  std::string ExpressionType() const override { return "Int"; }
 
- private:
+private:
   int _value;
 };
 
-class PrefixExpression : public Expression {
- public:
-  PrefixExpression(TokenType t, std::unique_ptr<Expression> value)
-    : _prefix_type(t), _expression_value(std::move(value)) {}
-
-  const std::unique_ptr<Expression>& expression_value() const {return _expression_value;}
-  TokenType prefix_type() const {return _prefix_type;}
+class BinaryExpression : public Expression {
+public:
+  BinaryExpression(TokenType t, std::unique_ptr<Expression> left,
+                   std::unique_ptr<Expression> right)
+      : _operation(t), _left(std::move(left)), _right(std::move(right)) {}
 
   std::unique_ptr<Literal> Evaluate() const override;
-  std::string ExpressionType() const override {return "Prefix";}
+  std::string ExpressionType() const override { return "Binary"; }
 
- private:
+private:
+  TokenType _operation;
+  std::unique_ptr<Expression> _left;
+  std::unique_ptr<Expression> _right;
+};
+
+class PrefixExpression : public Expression {
+public:
+  PrefixExpression(TokenType t, std::unique_ptr<Expression> value)
+      : _prefix_type(t), _expression_value(std::move(value)) {}
+
+  const std::unique_ptr<Expression> &expression_value() const {
+    return _expression_value;
+  }
+  TokenType prefix_type() const { return _prefix_type; }
+
+  std::unique_ptr<Literal> Evaluate() const override;
+  std::string ExpressionType() const override { return "Prefix"; }
+
+private:
   TokenType _prefix_type;
   std::unique_ptr<Expression> _expression_value;
 };
 
-
 // identifiers with their type and storage
 class Declaration : public Instruction {
- public:
+public:
   Declaration(std::unique_ptr<Identifier> ident, Type type)
-    : _identifier(std::move(ident)), _type(type) {}
+      : _identifier(std::move(ident)), _type(type) {}
 
-  std::string TypeInstruction() const override {
-    return "Declaration";
-  };
-  const std::unique_ptr<Identifier>& identifier() const {
-    return _identifier;
-  }
-  Type type() const {
-    return _type;
-  }
+  std::string TypeInstruction() const override { return "Declaration"; };
+  const std::unique_ptr<Identifier> &identifier() const { return _identifier; }
+  Type type() const { return _type; }
 
- private:
+private:
   Type _type;
   std::unique_ptr<Identifier> _identifier;
 };
 
 class Program {
- public:
+public:
   Program() = default;
 
-  const std::vector<std::unique_ptr<Instruction>>& instructions() const {
+  const std::vector<std::unique_ptr<Instruction>> &instructions() const {
     return _instructions;
   }
   void add_instruction(std::unique_ptr<Instruction> instruction) {
     _instructions.push_back(std::move(instruction));
   }
 
- private:
+private:
   std::vector<std::unique_ptr<Instruction>> _instructions;
 };
 
-class ReturnStatement : public Statement{
- public:
+class ReturnStatement : public Statement {
+public:
   ReturnStatement(std::unique_ptr<Expression> return_value)
-    : _return_value(std::move(return_value)) {}
+      : _return_value(std::move(return_value)) {}
 
   std::string TypeInstruction() const override { return "ReturnStatement"; };
 
-  const std::unique_ptr<Expression>& return_value() const { return _return_value; };
+  const std::unique_ptr<Expression> &return_value() const {
+    return _return_value;
+  };
 
- private:
+private:
   std::unique_ptr<Expression> _return_value;
 };
 
 class FunctionDeclaration : public Declaration {
- public:
+public:
   FunctionDeclaration(std::unique_ptr<Identifier> ident, Type type)
-    : Declaration(std::move(ident), type) {}
+      : Declaration(std::move(ident), type) {}
 
   std::string TypeInstruction() const override {
     return "FunctionDeclaration";
   };
 
-  const std::vector<std::unique_ptr<Instruction>>& instructions() const {
+  const std::vector<std::unique_ptr<Instruction>> &instructions() const {
     return _instructions;
   }
 
@@ -167,7 +172,6 @@ class FunctionDeclaration : public Declaration {
     _instructions.push_back(std::move(instruction));
   }
 
-
- private:
+private:
   std::vector<std::unique_ptr<Instruction>> _instructions;
 };
